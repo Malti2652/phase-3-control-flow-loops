6d24766962b0c34f968de5ddec3cf7ff293c6a22
Eddington Rowellis <eddingtonibi@gmail.com>
2023-03-13 15:03:31 +0300

    commit tree 40c93b648de84e876df3cd36c79d0707810a827b

--- looping.rb
+++ looping.rb
@@ -1,7 +1,15 @@
 def happy_new_year
   # your code here
+  i = 10
+  while i > 0
+    puts i
+    i -= 1
+  end
+  puts "Happy New Year!"
 end
 
+end
+ 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
 def fizzbuzz(num)
   if num % 3 == 0 && num % 5 == 0
